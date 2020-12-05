59310138cfb3c6c942d4c331f6f435fa572e446e
tej0 <tejaDavuluri007@gmail.com>
2020-12-05 12:26:25 +0530

    Add Delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,29 @@
+const { MongoClient,ObjectId } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unsble to connect to MongoDB server');
+    }
+    console.log('Connect to MongoDB server');
+
+    //delete many
+    // db.collection('Todos').deleteMany({text:'eat brakfast'}).then((result)=>{
+    //     console.log(result);
+    // })
+    //delete One
+    // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((result) => {
+    //     console.log(result);
+    // });
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({ completed:false }).then((result) => {
+    //     console.log(result);
+    // });
+    //db.close();
+
+    db.collection('user').deleteMany({name:'div' }).then((result) => {
+        console.log(result);
+    });
+    db.collection('user').findOneAndDelete({_id:new ObjectId("5fc9da45d7e7a11b38fa102d")}).then((result)=>{
+        console.log(JSON.stringify(result,undefined,2));
+    })
+});
