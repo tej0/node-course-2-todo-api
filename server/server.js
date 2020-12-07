ca5a64c69ea34ad3a28fdb85faff4bb5f41d3d9d
tej0 <tejaDavuluri007@gmail.com>
2020-12-07 22:31:43 +0530

    addig Post And test-post

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/Todo');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Sarted on port 3000');
+});
+
+module.exports = {app};
