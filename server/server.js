92471059ab0aa09f96136d035eba4f9a3b8d47a5
tej0 <tejaDavuluri007@gmail.com>
2020-12-08 22:53:44 +0530

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { ObjectID } = require('mongodb');
 
 
 var app = express();
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -35,24 +36,24 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo)=>{
-        if(!todo){
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
             return res.status(404).send();
         }
-        res.send({todo});
-    }).catch((e)=>{
+        res.send({ todo });
+    }).catch((e) => {
         res.status(400).send();
     })
 
 });
 
 
-app.listen(3000, () => {
-    console.log('Sarted on port 3000');
+app.listen(port, () => {
+    console.log(`Sarted up at  port ${port}`);
 });
 
 module.exports = { app };
