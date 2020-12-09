639fffe27a75b90dd29f75e3fd5a5e3a20338cfd
tej0 <tejaDavuluri007@gmail.com>
2020-12-09 20:47:42 +0530

    created test and local DB separate

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config.js')
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -133,3 +133,41 @@ describe('DELETE /todos/:id', () => {
     });
 
 });
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'this should be the new text';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeFalsy();
+            })
+            .end(done);
+    });
+    it('should clear completeAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'this should be the new text!!!';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+            })
+            .end(done);
+    });
+});
